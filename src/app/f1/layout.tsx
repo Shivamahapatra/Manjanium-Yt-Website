ff45db08bfea3358228a8e78d4dc54452b673f7e
Snehasish <96766456+Shivamahapatra@users.noreply.github.com>
2026-06-19 15:05:17 +0530

    Update layout.tsx

--- src/app/f1/layout.tsx
+++ src/app/f1/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Youtube } from "lucide-react";
+import { IconBrandYoutube } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
