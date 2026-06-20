f2c70e6ddb1983db72cb3b493a6ab9251cc4d7b9
Snehasish <96766456+Shivamahapatra@users.noreply.github.com>
2026-06-20 17:18:54 +0530

    Update route.ts

--- src/app/api/auth/webhook/route.ts
+++ src/app/api/auth/webhook/route.ts
@@ -5,10 +5,10 @@ import { supabase } from '@/lib/supabase'
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error('Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
   // Get the headers
