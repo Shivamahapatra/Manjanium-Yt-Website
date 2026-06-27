2346eea4f31ab43f8a7906143d5591820f1d7714
Snehasish <96766456+Shivamahapatra@users.noreply.github.com>
2026-06-27 21:08:10 +0530

    Update GameTrack.tsx

--- src/components/simulator/GameTrack.tsx
+++ src/components/simulator/GameTrack.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useMemo, useRef, useEffect } from 'react'
-import { RigidBody } from '@react-three/rapier'
+import { RigidBody, CuboidCollider } from '@react-three/rapier'
 import * as THREE from 'three'
 import { useGamePhysics } from '@/hooks/useGamePhysics'
 import { useSimulatorGame } from '@/hooks/useSimulatorGame'
@@ -52,7 +52,8 @@ export default function GameTrack() {
   return (
     <>
       {/* Ground/Asphalt */}
-      <RigidBody type="fixed" colliders="cuboid">
+      <RigidBody type="fixed" colliders={false}>
+        <CuboidCollider args={[200, 0.1, 200]} position={[0, -0.1, 0]} />
         <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
           <planeGeometry args={[400, 400]} />
           <meshStandardMaterial color="#1a1a1a" roughness={0.8} />
